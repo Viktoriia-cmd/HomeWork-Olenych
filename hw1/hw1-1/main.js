9fe1bdd39ff9a48c13fe3ce2f8bcdee6891d8f5a
Viktoriia-cmd <v.o.olenych@gmail.com>
2024-11-05 11:49:58 +0100

    homework 1

---
+++ hw1/hw1-1/main.js
@@ -0,0 +1,16 @@
+//first task
+
+let a = "hello";
+console.log(a);
+let b = "owu";
+console.log(b);
+let c = "ua";
+console.log(c);
+let d = 1;
+console.log(d);
+let e = 10;
+console.log(e);
+let f = -999;
+console.log(f);
+
+
---
+++ hw1/hw1-2/main.js
@@ -0,0 +1,5 @@
+let firstName = 'Viktoriia';
+let middleName = 'Oleksandrivna';
+let lastName ='Olenych';
+let msg = `${firstName} ${middleName} ${lastName}`;
+console.log(msg);
