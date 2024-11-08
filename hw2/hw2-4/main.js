4e8caff2f31ba4b18cb694d9e3de766a3b976157
Viktoriia-cmd <v.o.olenych@gmail.com>
2024-11-08 13:11:39 +0100

    Task 4

---
+++ hw2/hw2-4/main.js
@@ -0,0 +1,23 @@
+
+let users = [
+    {name: `Masha`, username: `masha123`, password: '1111'},
+    {name: `Nina`, username: `nina123`, password: `2222`},
+    {name: `Nik`, username: `nik345`, password: `3333`},
+    {name: `Jon`, username: `jon765`, password: `4444`},
+    {name: `Alice`, username: `alice389`, password: `5555`},
+    {name: `Bob`, username: `b845`, password: `6666`},
+    {name: `Jack`, username: `jc2987`, password: `7777`},
+    {name: `Henry`, username: `hr8376`, password: `8888`},
+    {name: `Emma`, username: `emma536`, password: `9999`},
+    {name: `Irina`, username: `ira467`, password: `1010`},
+]
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
