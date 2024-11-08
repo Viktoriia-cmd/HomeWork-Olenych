c5c40d3b25f29c86cc3ebcca205de0568d16b981
Viktoriia-cmd <v.o.olenych@gmail.com>
2024-11-08 13:37:48 +0100

    Task 5

---
+++ hw2/hw2-5/main.js
@@ -0,0 +1,9 @@
+let temp = [
+    {day:`monday`, values:[6, 17, 5]},
+    {day:`tuesday`, values:[3, 15, 1]},
+    {day:`wednesday`, values:[5, 21, 7]},
+    {day:`thursday`, values:[9, 22, 8]},
+    {day:`friday`, values:[6, 18, 7]},
+    {day:`saturday`, values:[8, 19, 6]},
+    {day:`sunday`, values:[7, 16, 3]},
+]
