ca26f0ae2970acd1770a922cb37e57d1f0c1c65b
Viktoriia-cmd <v.o.olenych@gmail.com>
2024-11-08 11:56:46 +0100

    Task 3

---
+++ hw2/hw2-3/main.js
@@ -0,0 +1,45 @@
+let book1 = {
+    title: `Monday starts on Saturday`,
+    pageCount: 320,
+    genre: `Fantasy`,
+    authors:[
+        {
+            name: `Arkady Strugatsky`,
+            age: 66
+        },
+        {
+            name: `Boris Strugatsky`,
+            age: 79
+        }
+    ]
+}
+let book2 = {
+    title: `The Art of Systems Thinking`,
+    pageCount: 234,
+    genre: `social psychology`,
+    authors:[
+        {
+            name: `Joseph O'Connor`,
+            age: 61
+        },
+        {
+            name: `Ian MaDermott`,
+            age: 65
+        }
+    ]
+}
+let book3 = {
+    title: `It's hard to be a god`,
+    pageCount: 256,
+    genre: `Fantasy`,
+    authors:[
+        {
+            name: `Arkady Strugatsky`,
+            age: 66
+        },
+        {
+            name: `Boris Strugatsky`,
+            age: 79
+        }
+    ]
+}
