ca8285c5f13464bc1e8f0e77c80582fdf462342b
Viktoriia-cmd <v.o.olenych@gmail.com>
2024-11-08 10:09:41 +0100

    Task 1

---
+++ hw2/hw1-1/main.js
@@ -0,0 +1,13 @@
+let arr;
+arr = [5, 10, 15, 30, 60, 120, 240, 480, 960, -999];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
