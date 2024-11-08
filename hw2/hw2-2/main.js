28b2c8276d7abe83f619adcb70cc90281b907cb7
Viktoriia-cmd <v.o.olenych@gmail.com>
2024-11-08 11:10:36 +0100

    Task 2

---
+++ hw2/hw2-2/main.js
@@ -0,0 +1,4 @@
+let book1 = {title: `Futurologist`, pageCount: 527, genre: `Fantasy`};
+let book2 = {title: `The Fall of the House of Usher`, pageCount: 425, genre: `Horror`};
+let book3 = {title: `Sex & The City`, pageCount: 366, genre: `Fiction`};
+
